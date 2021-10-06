3b50dec2a12f892f878f2c6f3e4c99c12f3ad18d
osuossu8 <osuosuossu18@gmail.com>
2021-10-06 14:48:18 +0000

    006

--- exp/006.py
+++ exp/006.py
@@ -261,6 +261,25 @@ class RMSELoss(torch.nn.Module):
         return loss
 
 
+def rand_bbox(size, lam):
+    W = size[2]
+    H = size[3]
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = np.int(W * cut_rat)
+    cut_h = np.int(H * cut_rat)
+
+    # uniform
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
+
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+    return bbx1, bby1, bbx2, bby2
+
+
 def mixup(data, targets, alpha):
     indices = torch.randperm(data.size(0))
     shuffled_data = data[indices]
