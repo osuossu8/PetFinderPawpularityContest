2213ed46f467aa726472bae92785fd3296111685
osuossu8 <osuosuossu18@gmail.com>
2021-12-11 01:28:44 +0000

    stratifiedgroupkfold

--- exp/adoption_speed_prediction.py
+++ exp/adoption_speed_prediction.py
@@ -360,16 +360,46 @@ def calc_cv(model_paths):
     print(oof_df.shape)
 
 
+class StratifiedGroupKFold():
+    def __init__(self, n_splits=5):
+        self.n_splits = n_splits
+
+    def split(self, X, y=None, groups=None):
+        fold = pd.DataFrame([X, y, groups]).T
+        fold.columns = ['X', 'y', 'groups']
+        fold['y'] = fold['y'].astype(int)
+        g = fold.groupby('groups')['y'].agg('mean').reset_index()
+        fold = fold.merge(g, how='left', on='groups', suffixes=('', '_mean'))
+        fold['y_mean'] = fold['y_mean'].apply(np.round)
+        fold['fold_id'] = 0
+        for unique_y in fold['y_mean'].unique():
+            mask = fold.y_mean == unique_y
+            selected = fold[mask].reset_index(drop=True)
+            cv = GroupKFold(n_splits=n_splits)
+            for i, (train_index, valid_index) in enumerate(
+                    cv.split(range(len(selected)), y=None, groups=selected['groups'])):
+                selected.loc[valid_index, 'fold_id'] = i
+            fold.loc[mask, 'fold_id'] = selected['fold_id'].values
+
+        for i in range(self.n_splits):
+            indices = np.arange(len(fold))
+            train_index = indices[fold['fold_id'] != i]
+            valid_index = indices[fold['fold_id'] == i]
+            yield train_index, valid_index
+
+
 def create_folds(data, num_splits):
     # we create a new column called kfold and fill it with -1
     data["kfold"] = -1
 
     # initiate the kfold class from model_selection module
-    kf = model_selection.StratifiedKFold(n_splits=num_splits, shuffle=True, random_state=42)
+    # kf = model_selection.StratifiedKFold(n_splits=num_splits, shuffle=True, random_state=42)
+
+    kf = StratifiedGroupKFold(n_splits=n_splits)
 
     # fill the new kfold column
     # note that, instead of targets, we use bins!
-    for f, (t_, v_) in enumerate(kf.split(X=data, y=data.AdoptionSpeed.values)):
+    for f, (t_, v_) in enumerate(kf.split(X=range(len(data)), y=data.AdoptionSpeed.values, groups=data.RescuerID.values)):
         data.loc[v_, 'kfold'] = f
 
     # return dataframe with folds
@@ -395,6 +425,8 @@ train['ImagePath'] = train['PetID'].map(lambda x: f'input/train_images/{x}-1.jpg
 # create folds
 NUM_SPLITS = 5
 train = create_folds(train, num_splits=NUM_SPLITS)
+
+print(train['AdoptionSpeed'].value_counts())
 
 print(train.shape)
 print(train.head())
